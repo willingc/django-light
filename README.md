4e1e26c176f8d82fa75ce5113586af1a70352451
Carol Willing <carolcode@willingconsulting.com>
2014-08-01 06:00:45 -0700

    Commit initial template files

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-django-light
-============
+This is a lightweight Django template based on the book, "Lightweight Django"
+by Julia Elman and Mark Lavin.
 
-A lightweight Django project template based on "Lightweight Django" bu Julia Elman and Mark Lavin.
+Project Template
+----------------
+To create a new project based on the current simple project template:
+
+    $django-admin.py startproject foo --template=project_name
+
+To run the application:
+
+    $python foo.py runserver
+
+
+Misc
+----
+To start gunicorn:
+    gunicorn hello --log-file=_
+
+To start simple runserver:
+    python hello.py runserver
---
+++ project_name/project_name.py
@@ -0,0 +1,39 @@
+# Must execute settings first to avoid unhappy execution
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+)
+
+# Setting up the url pattern, wsgi server, basic application
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+application = get_wsgi_application()
+
+# make it executable from the command line
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
